b8f8ecff292cdd4abf2b137cd3d61ec43f5a735d
Sae Shikiji <saeshikiji@gmail.com>
2021-08-12 23:11:26 +0900

    Made page responsive

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -4,7 +4,7 @@ const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["Movie Watch List", "Make it your own!"],
     typeSpeed: 80,
-    loop: true
+    loop: true,
   });
 }
 
